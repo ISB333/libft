4eaf699c39bf8f74b911a6709accb5efe4ff5d7d
Alexandre Desille <adesille@k0r3p1.42mulhouse.fr>
2023-10-27 13:44:10 +0200

    libft 27-10-23_13.45

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,88 +6,94 @@
 /*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 22:11:00 by adesille          #+#    #+#             */
-/*   Updated: 2023/10/27 01:23:18 by adesille         ###   ########.fr       */
+/*   Updated: 2023/10/27 13:43:16 by adesille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int digitcount(int n)
+int	digitcount(int n)
 {
-    int temp;
-    int digitlen;
+	int	temp;
+	int	digitlen;
 
-    temp = n;
-    digitlen = 1;
-    if (temp < 0)
-        temp = -temp;
-    while (temp >= 10)
-    {
-        temp /= 10;
-        digitlen++;
-    }
-    return (digitlen);
+	temp = n;
+	digitlen = 1;
+	if (temp < 0)
+		temp = -temp;
+	while (temp >= 10)
+	{
+		temp /= 10;
+		digitlen++;
+	}
+	return (digitlen);
 }
 
-char *ft_putnbr(int n, int digitlen, char *numbers)
+char	*ft_putnbr_pos(int n, int digitlen)
 {
-    int end = digitlen - 1;
-    int trueend = digitlen;
+	int	end;
+	int	trueend;
+	char	*numbers;
 
-    if (n < 0)
-    {
-        numbers[0] = '-';
-        n = -n;
-    }
-    while (end >= 0)
-    {
-        numbers[end] = n % 10 + '0';
-        n /= 10;
-        end--;
-    }
-    numbers[trueend] = '\0'; 
-    return (numbers);
+	numbers = malloc(digitlen + 1);
+	if (numbers == NULL)
+		return (NULL);
+	end = digitlen - 1;
+	trueend = digitlen;
+	while (end >= 0)
+	{
+		numbers[end] = n % 10 + '0';
+		n /= 10;
+		end--;	
+	}
+	numbers[trueend] = '\0';
+	return (numbers);
 }
 
-char *ft_itoa(int n)
+char	*ft_putnbr_neg(int n, int digitlen)
 {
-    int digitlen;
-    char *numbers;
+	int	end;
+	int	trueend;
+	char	*numbers;
 
-    if (n == -2147483648)
-        return ("-2147483648");
-    digitlen = digitcount(n);
-    if (n < 0)
-        numbers = malloc(digitlen + 2);
-    else
-        numbers = malloc(digitlen + 1);
-    if (numbers == NULL)
-        return (NULL);
+	numbers = malloc(digitlen + 1);
+	if (numbers == NULL)
+		return (NULL);
+	end = digitlen - 1;
+	trueend = digitlen;
+	numbers[0] = '-';
+	n = -n;	
+	while (end > 0)
+	{
+		numbers[end] = n % 10 + '0';
+		n /= 10;
+		end--;
+	}
+	numbers[trueend] = '\0';
+	return (numbers);
+}
 
-    numbers = ft_putnbr(n, digitlen, numbers);
-    return (numbers);
+char	*ft_itoa(int n)
+{
+	char *result;
+
+	int		digitlen;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	digitlen = digitcount(n);
+	if (n < 0)
+		result = ft_putnbr_neg(n, digitlen + 1);
+	else
+		result = ft_putnbr_pos(n, digitlen);
+	return (result);
 }
 /*
 int main(void)
 {
-	// printf("%s\n", ft_itoa(1000034));
-	printf("%s\n", ft_itoa(-10004));
-	printf("%s\n", ft_itoa(2147483647));
-	printf("%s\n", ft_itoa(-2147483648));
+	printf("%s\n", ft_itoa(-5859));
+	printf("%s\n", ft_itoa(-369));
 	printf("%s\n", ft_itoa(-623));
-	printf("%s\n", ft_itoa(156));
 	printf("%s\n", ft_itoa(-0));
-}
-*/
-/*
-int main(void)
-{
-    char *result = ft_itoa(-10004);
-    if (result != NULL) {
-        printf("%s\n", result);
-        free(result); // Remember to free allocated memory
-    }
-
-    return 0;
 }
 */
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 01:25:07 by adesille          #+#    #+#             */
-/*   Updated: 2023/10/27 02:08:53 by adesille         ###   ########.fr       */
+/*   Updated: 2023/10/27 12:23:31 by adesille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,21 +30,23 @@ char	*ft_split(char const *s, char c)
 
 int main()
 {
-    const char *input_string = "Hello World! How are you?";
-    char **result = ft_split(input_string, ' ');
+	const char	*input_string = "Hello World! How are you?";
+    char		**result = ft_split(input_string, ' ');
 
-    if (result == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
+    if (result == NULL) 
+	{
+		printf("Memory allocation failed.\n");
+		return 1;
+	}
 
-    int i = 0;
-    while (result[i] != NULL) {
-        printf("%s\n", result[i]);
-        free(result[i]);
-        i++;
-    }
+	int i = 0;
+	while (result[i] != NULL) 
+	{
+		printf("%s\n", result[i]);
+		free(result[i]);
+		i++;
+	}
 
-    free(result);
-    return 0;
+	free(result);
+	return 0;
 }
