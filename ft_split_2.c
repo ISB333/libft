21867b78bfa129d9e503218913e2e172a2015f45
Alexandre Desille <adesille@k0r3p1.42mulhouse.fr>
2023-10-28 18:18:01 +0200

    libft 28-10-23_18.18

--- ft_split_2.c
+++ ft_split_2.c
@@ -6,7 +6,7 @@
 /*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 01:25:07 by adesille          #+#    #+#             */
-/*   Updated: 2023/10/27 19:38:25 by adesille         ###   ########.fr       */
+/*   Updated: 2023/10/28 18:04:45 by adesille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@
 /// 4 /// 	strlcpy[str[i], s, colsize]
 ///////// rowsnbr--;
 
+static char	**ft_allocncpy(char **array, const char *s, size_t size)
+{
+	size_t	s_len;
+	int j;
+
+	j = 0;
+	s_len = ft_strlen(s);
+	if (!array || !s || !size)
+		return (array);
+	array[j] = (char *)malloc((size - 1 * sizeof(char)));
+	if (array[j] == NULL)
+		return (0);
+	while (*s && --size)
+		*array[j]++ = *s++;
+	*array[j] = '\0';
+	j++;
+	return (&array[j]);
+}
+
 static int	ft_del_rows(const char *s, int c)
 {
 	int	i;
@@ -31,42 +50,44 @@ static int	ft_del_rows(const char *s, int c)
 	{
 		if (s[i] == (char)c)
 			rows++;
+			
 		i++;
 	}
 	return (rows);
 }
 
-static int	ft_rows_size2(const char *s, int c)
+static int	ft_rows_size(char **array, const char *s, int c, int rows)
 {
 	int	i;
-	int rows;
+	int j;
+	int tempi;
+	int token;
 
-	rows = 0;
 	i = 0;
+	j = 0;
+	tempi = 0;
+	token = 0;
 	while (s[i])
 	{
 		if (s[i] == (char)c)
-			return(ft_strlen(&s[i]));
+		{
+			token++;
+			if (token > 0)
+			{
+				ft_allocncpy(&array[i], &s[tempi], i - tempi - 1);
+			}
+			tempi = i;
+		}
 		i++;
+		if (s[i] == '\0')
+		{
+			array[j] = (char *)malloc((i - tempi - 1) * sizeof(char));
+			ft_allocncpy(&array[i], &s[tempi], i - tempi - 1);
+		}
 	}
-	return(0);
-}
-
-static int	ft_rows_size(const char *s, int c)
-{
-	int	i;
-	int rows_size;
-	int slen;
-
-	rows_size = 0;
-	i = 0;
-	slen = ft_strlen((char *)s);
-	while(*s)
-	{
-		s++;
-		printf("%d\n", slen - ft_rows_size2(s, c));
-	}
-	return (rows_size);
+	for (int i = 0; i < rows; i++)
+        printf("%s is 1\n", array[i]);
+	return (j);
 }
 
 char	*ft_split(char const *s, char c)
@@ -74,11 +95,11 @@ char	*ft_split(char const *s, char c)
 	char **array;
 	int	rows;
 
-	rows = ft_del_rows(s, c);
+	rows = ft_del_rows(s, c) + 1;
 	array = (char **)malloc(rows * sizeof(char *));
 	if (array == NULL)
 		return(NULL);
-	ft_rows_size(s, c);
+	ft_rows_size(array, s, c, rows);
 	return ((char *) s);
 }
 
