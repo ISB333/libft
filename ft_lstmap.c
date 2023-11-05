e198ff1a65f89f8b417fc335f5e510ec3c332c74
Alexandre Desille <adesille@k0r2p7.42mulhouse.fr>
2023-11-05 17:12:57 +0100

    libft 05-11-23_17.15

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,25 +6,29 @@
 /*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 15:46:12 by isb3              #+#    #+#             */
-/*   Updated: 2023/11/05 16:20:03 by adesille         ###   ########.fr       */
+/*   Updated: 2023/11/05 17:12:09 by adesille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// llocate temp = ft
+// create new node with lstnew
+// if !new = lstclear and del
+// put new node at the end with lstaddback
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*current;
+	t_list	*temp;
 	t_list *new;
 
-	current = lst;
-	while (current)
+	temp = lst;
+	while (lst)
 	{
-		new->content = f(current->content);
-		new->next = current->next;
+		new->content = f(temp->content);
 		if(!new)
 			del(new)
-		current = current->next;
-
+		temp = temp->next;
+		ft_lstadd_back
 	}
 }
